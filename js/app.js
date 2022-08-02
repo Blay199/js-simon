e0a34317417d9ab3e07fe6fe7cc3355411b5f80f
Blay199 <a.sorrentino03@gmail.com>
2022-08-02 16:24:41 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// array random vuoto
+let nRandom = []
+
+// ciclo while finchè arrayRandom < 5  
+while (nRandom < 5) {
+    // generare numeri random 
+    let nGen = Math.floor(Math.random(1))
+    // se array NON include numero random
+        // pushare in array 
+    
+}
+// alert con numeri random
+
+// timeout 30000 ms 
+    // Array utente vuoto
+    // ciclo while finchè lunghezza arrayUtente è < 5 
+        // PARSEprompt di un numero
+        // se è un numero 
+            // pusho nel array utente
+
+    // ciclo for 
+        // se arrayRandom i è uguale arrayUtente i 
+            // alert "il confronto" + (i + 1) + "è corretto"
+            // alert `il confronto ${i + 1} è corretto`
+        // altrimenti 
+            // alert `il confronto i + 1 non è corretto`
