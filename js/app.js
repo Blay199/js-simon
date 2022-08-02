d7c6965f6463c3d4a81d0e83eef2d422bfbdfc25
Blay199 <a.sorrentino03@gmail.com>
2022-08-02 17:20:19 +0200

    perchè non mi prende la chiusura del timeout

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let nRandom = []
 // ciclo while finchè arrayRandom < 5  
 while (nRandom.length < 5) {
     // generare numeri random 
-    let nGen = Math.floor(Math.random(1))
+    let nGen = Math.floor(Math.random() * 100 + 1)
     // se arrayRandom NON include numero random
     if (!nRandom.includes(nGen))
         // pushare in array 
@@ -13,16 +13,30 @@ while (nRandom.length < 5) {
 // alert con numeri random
 alert(nRandom)
 
+
+// Array utente vuoto
+let playerN =  []
 // timeout 30000 ms 
-    // Array utente vuoto
-    // ciclo while finchè lunghezza arrayUtente è < 5 
+setTimeout(() => {    
+    // ciclo while finchè lunghezza arrayUtente è < 5
+    while (playerN < 5) {
         // PARSEprompt di un numero
+        let playerGuess = parseInt(prompt('Ricordi i numeri?'))
         // se è un numero 
+        if (!isNaN(playerGuess) ) {
             // pusho nel array utente
+            playerN.push(playerGuess)           
+        }        
+    }
 
     // ciclo for 
+    // for (let i = 0; i < array.length; i++) {
+    //     const element = array[i];
         // se arrayRandom i è uguale arrayUtente i 
             // alert "il confronto" + (i + 1) + "è corretto"
             // alert `il confronto ${i + 1} è corretto`
         // altrimenti 
             // alert `il confronto i + 1 non è corretto`
+        
+    }
+}, 1000);
